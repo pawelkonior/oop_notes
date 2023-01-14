cd132f181869ae4cd08b942d9723e0a9b1adf67e
Pawel Konior <pawel.konior@gmail.com>
2023-01-14 13:10:17 +0100

    auto implementation using oop paradigm

---
+++ auto/auto_oop.py
@@ -0,0 +1,45 @@
+class Auto:
+    def __init__(self, model, max_speed):
+        self.model = model
+        self.engine = False
+        self.speed = 0
+        self.max_speed = max_speed
+
+    def start_engine(self):
+        if not self.engine:
+            self.engine = True
+            print('Silnik odpalony')
+        else:
+            print('Silnik już był odpalony')
+
+    def stop_engine(self):
+        if self.speed == 0:
+            self.engine = False
+            print('silnik zgaszony')
+
+        else:
+            print('zatrzymaj auto wpierw 😎')
+
+    def speed_up(self, amount):
+        if self.engine:
+            self.speed = min(self.speed + amount, self.max_speed)
+            print(f'jedziesz z prędkością {self.speed}')
+        else:
+            print('odpal silnik wpierw')
+
+    def slow_down(self, amount):
+        self.speed = max(self.speed - amount, 0)
+        print(f'jedziesz z prędkością {self.speed}')
+
+
+bmw = Auto('e46', 160)
+# Auto.speed_up(bmw, 20)
+bmw.speed_up(20)
+bmw.start_engine()
+bmw.speed_up(20)
+bmw.speed_up(50)
+bmw.speed_up(200)
+bmw.stop_engine()
+bmw.slow_down(50)
+bmw.slow_down(250)
+bmw.stop_engine()
